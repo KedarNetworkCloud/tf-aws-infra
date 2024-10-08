af86f1be19f3738726d0a159c3cf7ab289ef08e2
aptekedar <apte.k@northeastern.edu>
2024-10-07 20:36:14 -0400

    Terraform code push

--- main.tf
+++ main.tf
@@ -2,3 +2,134 @@ provider "aws" {
   profile = var.aws_profile
   region  = var.aws_region
 }
+
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block       = var.vpc_cidr
+
+  tags = {
+    Name = var.vpc_name  // This uses the VPC name from our .tfvars file. This is the name that would show up in the AWS console.
+  }
+}
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = var.public_subnet_cidr_1
+  availability_zone       = var.subnet_1_zone
+
+  tags = {
+    Name = "${var.vpc_name}-public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = var.public_subnet_cidr_2
+  availability_zone       = var.subnet_2_zone
+
+  tags = {
+    Name = "${var.vpc_name}-public-subnet-2"
+  }
+}
+
+resource "aws_subnet" "public_subnet_3" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = var.public_subnet_cidr_3
+  availability_zone       = var.subnet_3_zone
+
+  tags = {
+    Name = "${var.vpc_name}-public-subnet-3"  
+  }
+}
+
+// Create Private Subnets
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = var.private_subnet_cidr_1
+  availability_zone = var.subnet_1_zone
+
+  tags = {
+    Name = "${var.vpc_name}-private-subnet-1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = var.private_subnet_cidr_2
+  availability_zone = var.subnet_2_zone
+
+  tags = {
+    Name = "${var.vpc_name}-private-subnet-2"
+  }
+}
+
+resource "aws_subnet" "private_subnet_3" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = var.private_subnet_cidr_3
+  availability_zone = var.subnet_3_zone
+
+  tags = {
+    Name = "${var.vpc_name}-private-subnet-3"
+  }
+}
+
+// Create an Internet Gateway
+resource "aws_internet_gateway" "internetGateway" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "${var.vpc_name}-internetgateway"
+  }
+}
+
+// Create a public route table
+resource "aws_route_table" "publicRouteTable" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internetGateway.id
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-private-subnet-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.publicRouteTable.id
+}
+
+resource "aws_route_table_association" "public_subnet_association_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.publicRouteTable.id
+}
+
+resource "aws_route_table_association" "public_subnet_association_3" {
+  subnet_id      = aws_subnet.public_subnet_3.id
+  route_table_id = aws_route_table.publicRouteTable.id
+}
+
+resource "aws_route_table" "privateRouteTable" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "${var.vpc_name}-private-subnet-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private_subnet_association_1" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.privateRouteTable.id
+}
+
+resource "aws_route_table_association" "private_subnet_association_2" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.privateRouteTable.id
+}
+
+resource "aws_route_table_association" "private_subnet_association_3" {
+  subnet_id      = aws_subnet.private_subnet_3.id
+  route_table_id = aws_route_table.privateRouteTable.id
+}
--- variables.tf
+++ variables.tf
@@ -7,3 +7,60 @@ variable "aws_region" {
   type        = string
   description = "AWS Region closest to us in Boston that we will use for VPC"
 }
+
+variable "vpc_name" {
+  type        = string
+  description = "Name of the VPC we created"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR Block for the VPC we created"
+}
+
+// Public Subnet CIDR Blocks
+variable "public_subnet_cidr_1" {
+  type        = string
+  description = "CIDR block for public subnet 1"
+}
+
+variable "public_subnet_cidr_2" {
+  type        = string
+  description = "CIDR block for public subnet 2"
+}
+
+variable "public_subnet_cidr_3" {
+  type        = string
+  description = "CIDR block for public subnet 3"
+}
+
+// Private Subnet CIDR Blocks
+variable "private_subnet_cidr_1" {
+  type        = string
+  description = "CIDR block for private subnet 1"
+}
+
+variable "private_subnet_cidr_2" {
+  type        = string
+  description = "CIDR block for private subnet 2"
+}
+
+variable "private_subnet_cidr_3" {
+  type        = string
+  description = "CIDR block for private subnet 3"
+}
+
+variable "subnet_1_zone" {
+  type        = string
+  description = "Availability zone for public subnet 1 and private subnet 1"   
+}
+
+variable "subnet_2_zone" {
+  type        = string
+  description = "Availability zone for public subnet 2 and private subnet 2"   
+}
+
+variable "subnet_3_zone" {
+  type        = string
+  description = "Availability zone for public subnet 3 and private subnet 3"   
+}

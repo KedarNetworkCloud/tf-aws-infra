3725b059075e6610feafb041510228986e46231d
aptekedar <apte.k@northeastern.edu>
2024-10-17 08:29:56 -0400

    Test Everything

--- main.tf
+++ main.tf
@@ -155,7 +155,7 @@ resource "aws_security_group" "application_security_webapp_kedar" {
   }
 
   ingress {
-  from_port   = 443     # Allow HTTPS
+from_port   = 443     # Allow HTTPS
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]

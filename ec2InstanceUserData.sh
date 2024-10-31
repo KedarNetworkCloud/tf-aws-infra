eee8f7692a5b580ddd25819d48498aa79e455dd2
aptekedar <apte.k@northeastern.edu>
2024-10-30 21:46:52 -0400

    Assignment push code

--- ec2InstanceUserData.sh
+++ ec2InstanceUserData.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# Update package list and install required software
+# Update package list
 sudo apt-get update -y
-#sudo apt-get install -y postgresql-client
 
 # Create the directory for the application if it doesn't exist
 sudo mkdir -p /opt/myapp/
@@ -14,11 +13,29 @@ DB_USERNAME=${DB_USERNAME}
 DB_PASSWORD=${DB_PASSWORD}
 DB_PORT=5432
 APP_PORT=8080
+S3_BUCKET_NAME=${S3_BUCKET_NAME}
+AWS_REGION=${aws_region}
 EOL
 
 # Set ownership and permissions for the .env file
 sudo chown csye6225:csye6225 /opt/myapp/.env
 sudo chmod 644 /opt/myapp/.env
+
+# Create logs directory if it doesn't exist
+sudo mkdir -p /opt/myapp/logs
+
+# Create the cloudwatch group and add users
+sudo groupadd cloudwatch || true
+sudo usermod -aG cloudwatch csye6225
+sudo usermod -aG cloudwatch cloudwatch-agent
+
+# Set ownership and permissions for the logs directory
+sudo chown -R csye6225:cloudwatch /opt/myapp/logs
+sudo chmod -R 775 /opt/myapp/logs
+sudo chmod g+s /opt/myapp/logs
+
+# Copy the CloudWatch Agent configuration file to the appropriate location
+sudo cp /opt/myapp/config/cloudwatch-config.json /opt/aws/amazon-cloudwatch-agent/etc/
 
 # Reload systemd to recognize new service
 sudo systemctl daemon-reload
@@ -27,5 +44,11 @@ sudo systemctl daemon-reload
 sudo systemctl enable kedarwebapp.service
 sudo systemctl start kedarwebapp.service
 
-# Log the status of the service
-sudo systemctl status kedarwebapp.service >> /var/log/user_data.log
+# Start the CloudWatch Agent using the provided configuration
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -s -c file:/opt/aws/amazon-cloudwatch-agent/etc/cloudwatch-config.json
+
+# Restart the CloudWatch Agent to apply the new configuration
+sudo systemctl restart amazon-cloudwatch-agent
+
+
+

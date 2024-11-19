9958ec5b9e87fb12ad45da09e4b1a5ee273bff22
aptekedar <apte.k@northeastern.edu>
2024-11-19 01:01:25 -0500

    Code push latest change

--- main.tf
+++ main.tf
@@ -663,7 +663,7 @@ resource "aws_lambda_function" "send_email" {
   environment {
     variables = {
       SENDGRID_API_KEY = var.SENDGRID_API_KEY
-      DOMAIN           = var.DEV_DOMAIN
+      DOMAIN           = var.DEMO_DOMAIN
       DB_HOST          = aws_db_instance.kedar_rds_instance.endpoint
       DB_HOST_NO_PORT  = replace(aws_db_instance.kedar_rds_instance.endpoint, ":5432", "")
       DB_PASSWORD      = var.RDS_INSTANCE_KEDAR_PASSWORD
@@ -821,9 +821,9 @@ resource "aws_s3_bucket" "lambda_bucket" {
 }
 
 resource "aws_s3_object" "lambda_layer_zip" {
-  bucket = aws_s3_bucket.lambda_bucket.bucket # References the S3 bucket name defined above
-  key    = "lambda-layer.zip"                 # File name directly in S3 root
-  source = "${path.module}/lambda-layer.zip"  # Path to ZIP file in Terraform root folder
+  bucket = aws_s3_bucket.lambda_bucket.bucket         # References the S3 bucket name defined above
+  key    = "lambda-layer.zip"                         # File name directly in S3 root
+  source = "${path.module}/lambda-layer.zip"          # Path to ZIP file in Terraform root folder
   //etag   = filemd5("${path.module}/lambda-layer.zip") # Ensures updates only if the file changes
 }
 
